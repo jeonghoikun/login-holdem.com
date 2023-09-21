5ef22b1bd8d2fe9d8ec8268e840ca364dfa1a3fd
jeonghoikun <jeong_hoi_kun.dev@quantumium.io>
2023-09-21 20:17:05 +0900

    updated

--- site/config.go
+++ site/config.go
@@ -38,7 +38,7 @@ func Init() {
 	c.Domain = "holdem-kingdom.com"
 	c.Author = "이사장"
 	c.Title = "강남 홀덤 압구정 홀덤 킹덤"
-	c.Description = "강남 홀덤 압구정 홀덤 킹덤은 홀덤 포커를 즐길 수 있는 공간으로, 카드 게임 팬들에게는 꼭 방문해야 하는 장소 중 하나입니다. 강남 홀덤 압구정 홀덤 곳은 서로 다른 분위기와 스타일을 가지고 있어 모든 유형의 홀덤 팬들에게 맞춤형 경험을 제공합니다."
+	c.Description = "강남 홀덤 압구정 홀덤 킹덤은 홀덤 포커를 즐길 수 있는 공간으로, 카드 게임 팬들에게는 꼭 방문해야 하는 장소 중 하나입니다. 강남 홀덤 압구정 홀덤 두 곳은 서로 다른 분위기와 스타일을 가지고 있어 모든 유형의 홀덤 팬들에게 맞춤형 경험을 제공합니다."
 	k := Keywords([]string{"강남홀덤", "압구정홀덤", "홀덤킹덤"})
 	c.Keywords = &k
 	c.DatePublished = date(2023, 9, 19)
@@ -46,7 +46,7 @@ func Init() {
 	// 업종마다 전화번호가 다른경우 store/store.go 파일의 setPhoneNumber 함수에서 하드코딩
 	c.PhoneNumber = "010-5900-9210"
 	c.SearchEngineConnection = &searchEngineConnection{
-		Google: "uB7ecmImPZewv2LwvJ68mQiRh31D14Tjdv2kuqqPiho",
+		Google: "ZoX_J-VyfV7xN7pc8tZpiSBPQbIfa0UMP0-MMYYUDHY",
 	}
 	Config = c
 }
