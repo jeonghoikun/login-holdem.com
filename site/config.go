e80966dec00d026fd41d2f45710644a97d8d19d9
jeonghoikun.dev <jeonghoikun.dev@gmail.com>
2024-04-05 00:05:40 +0900

    Phone Number Verify

--- site/config.go
+++ site/config.go
@@ -44,7 +44,7 @@ func Init() {
 	c.DatePublished = date(2023, 10, 16)
 	c.DateModified = date(2024, 1, 10)
 	// 업종마다 전화번호가 다른경우 store/store.go 파일의 setPhoneNumber 함수에서 하드코딩
-	c.PhoneNumber = "010-2378-6514"
+	c.PhoneNumber = "010-2662-4511"
 	c.SearchEngineConnection = &searchEngineConnection{
 		Google: "e9dafNYxmGa1DwOGWBXQ5T213mPPAxIBc0Urpn0qJMU",
 	}
